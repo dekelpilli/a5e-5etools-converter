107db9f11c1e5dc56bcaa0e403864eef13fb732c
Dekel Pilli <dekelpilli@gmail.com>
2022-01-27 17:02:16 +1100

    Initial commit; transfer without history

---
+++ src/a5e_5etools_converter/util.clj
@@ -0,0 +1,25 @@
+(ns a5e-5etools-converter.util
+  (:require [clojure.edn :as edn])
+  (:import (java.util.concurrent TimeUnit)
+           (java.util Date)))
+
+(defn ->num [s]
+  (try
+    (let [n (edn/read-string s)]
+      (when (number? n) n))
+    (catch Exception _)))
+
+(defn idx-in-bounds? [idx coll]
+  (<= idx (dec (count coll))))
+
+(defn- source-meta []
+  (let [now (->> (Date.) (inst-ms) (.toSeconds TimeUnit/MILLISECONDS))]
+    {:sources          [{:json         "LevelUpAdventurersGuideA5E"
+                         :abbreviation "A5E"
+                         :full         "Level Up: Adventurers Guide (A5E)"
+                         :url          "https://www.levelup5e.com/"
+                         :authors      ["Level Up"]
+                         :convertedBy  ["TODO DMs"]
+                         :version      "0.0.1"}]
+     :dateAdded        now
+     :dateLastModified now}))
